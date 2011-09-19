61a91f0045bb8f5e943992cefb5187ca8e192174
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2011-09-19 15:17:17 +0200

    bad link for the résumé

--- pelican.conf.py
+++ pelican.conf.py
@@ -18,7 +18,7 @@ SOCIAL = (
 ('twitter', 'https://twitter.com/#!chmduquesne'),
 ('github', 'https://github.com/chmduquesne'),
 ('about me', 'http://about.me/chmduquesne'),
-('resume', 'articles/documents/christophe-marie_duquesne_resume.pdf'),
+('resume', 'static/documents/christophe-marie_duquesne_resume.pdf'),
 )
 
 GITHUB_URL = "https://github.com/chmduquesne"
