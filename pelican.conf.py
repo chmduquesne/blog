85f52ea10805d0b33853ff78fd00736a936e0cf7
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2013-10-29 01:38:58 +0100

    switching to personal theme

--- pelican.conf.py
+++ pelican.conf.py
@@ -2,31 +2,13 @@
 
 AUTHOR = "Tof"
 SITENAME = "Ch-M.D"
-SITEURL = 'http://blog.chmd.fr'
-#SITEURL = 'http://chmd.fr/blog'
+#SITEURL = 'http://blog.chmd.fr'
+SITEURL = '/'
 TIMEZONE = "Europe/Paris"
 STATIC_PATHS = ['images', 'documents']
-THEME = "../pelican-themes/built-texts/"
-#THEME = "../pelican-themes/chunk/"
-#THEME = "../pelican-themes/fresh/"
-#THEME = "../pelican-themes/notmyidea-cms/"
-#THEME = "../pelican-themes/pelican-bootstrap3/"
-#THEME = "../pelican-themes/pelican-cait/"
-#THEME = "../pelican-themes/simple-bootstrap/"
-#THEME = "../pelican-themes/subtle/"
-#THEME = "../pelican-themes/svbtle/"
-#THEME = "../pelican-themes/basic/"
-#THEME = "../pelican-themes/waterspill/"
-
-
-# Social widget
-SOCIAL = (
-('twitter', 'https://twitter.com/#!chmduquesne'),
-('resume', 'http://resume.chmd.fr'),
-)
+THEME = "../small"
 
 GITHUB_URL = "https://github.com/chmduquesne"
 TWITTER_USERNAME = "chmduquesne"
 DISQUS_SITENAME = "chmd"
-GOOGLE_ANALYTICS = "UA-17659080-3"
-AUTHOR_BIO = ""
+FEED_DOMAIN = "http://blog.chmd.fr"
